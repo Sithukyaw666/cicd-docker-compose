a80513a88b1fe22c0702dcd4c4de03828b026fd3
Sithu <sithukyaw27500@gmail.com>
2025-02-17 15:59:04 +0630

    updated version

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello friend. Have a great day ahead!");
 });
+app.get('/ping', (req, res) => {
+  res.send('pong!')
+})
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
